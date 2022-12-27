fee2771cf9c4deb0d8a29445f19c49860be909a8
Santio <admin@santio.me>
2022-12-27 14:24:18 -0500

    Create fallback for fetching classes and packages

--- reflection/src/main/kotlin/me/santio/utils/reflection/Reflection.kt
+++ reflection/src/main/kotlin/me/santio/utils/reflection/Reflection.kt
@@ -6,21 +6,25 @@ import me.santio.utils.reflection.types.PackageReflection
 
 object Reflection {
     @JvmStatic
-    fun getClass(name: String): ClassReflection<*>? {
-        return try {
-            ClassReflection(Class.forName(name))
-        } catch (e: Exception) {
-            return null
+    fun getClass(vararg names: String): ClassReflection<*>? {
+        for (name in names) {
+            try {
+                return ClassReflection(Class.forName(name))
+            } catch (ignored: Exception) {}
         }
+
+        return null
     }
 
     @Suppress("DEPRECATION")
     @JvmStatic
-    fun getPackage(name: String): PackageReflection? {
-        return try {
-            PackageReflection(Package.getPackage(name))
-        } catch (e: Exception) {
-            return null
+    fun getPackage(vararg names: String): PackageReflection? {
+        for (name in names) {
+            try {
+                return PackageReflection(Package.getPackage(name))
+            } catch (ignored: Exception) {}
         }
+
+        return null
     }
 }
