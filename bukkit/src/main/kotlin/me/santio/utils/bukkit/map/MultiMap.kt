77aad34d7eb2bd8189ec01cd0adb7d86767a8a4d
Santio <admin@santio.me>
2023-01-01 15:16:37 -0500

    Let's try this again

--- bukkit/src/main/kotlin/me/santio/utils/bukkit/map/MultiMap.kt
+++ bukkit/src/main/kotlin/me/santio/utils/bukkit/map/MultiMap.kt
@@ -12,7 +12,7 @@ import kotlin.math.abs
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 class MultiMap {
 
-    private val matrix: MutableList<MutableList<CustomMap>> = mutableListOf()
+    private val matrix: MutableList<MutableList<CustomMap?>> = mutableListOf()
 
     fun generate(topLeft: Location, bottomRight: Location, direction: BlockFace): MultiMap {
         val blocks = topLeft to bottomRight blocks topLeft.world
@@ -26,21 +26,28 @@ class MultiMap {
             val map = CustomMap()
             itemFrame.setItem(map.item())
 
-            // Add to matrix, the first index being the x and the second the y
-            if (matrix.size <= x) matrix.add(mutableListOf())
-            matrix[x].add(map)
+            if (matrix.size <= x) matrix.add(mutableListOf<CustomMap?>().apply { repeat(y) { add(null) } })
+            matrix[x][y] = map
         }
 
         return this
     }
 
-    fun getPixel(x: Int, y: Int): Pixel {
+    fun maps(): List<List<CustomMap>>? {
+        if (matrix.flatten().any { it == null }) return null
+        return matrix.map { it.map { m -> m!! } }
+    }
+
+    fun getPixel(x: Int, y: Int): Pixel? {
+        val maps = maps() ?: return null
+
         val mapX = x / 128
         val mapY = y / 128
         val pixelX = x % 128
         val pixelY = y % 128
 
-        return Pixel(matrix[mapX][mapY], pixelX to pixelY)
+        if (maps.size <= mapX || maps[mapX].size <= mapY) return null
+        return Pixel(maps[mapX][mapY], pixelX to pixelY)
     }
 
     fun sphere(center: Pixel, radius: Int): List<Pixel> {
@@ -50,7 +57,8 @@ class MultiMap {
             for (y in -radius..radius) {
                 for (z in -radius..radius) {
                     if (x * x + y * y + z * z <= radius * radius) {
-                        pixels.add(getPixel(center.coordinates.first + x, center.coordinates.second + y))
+                        val pixel = getPixel(center.coordinates.first + x, center.coordinates.second + y) ?: continue
+                        pixels.add(pixel)
                     }
                 }
             }
@@ -63,7 +71,8 @@ class MultiMap {
 
         for (pX in x..x + width) {
             for (pY in y..y + height) {
-                pixels.add(getPixel(pX, pY))
+                val pixel = getPixel(pX, pY) ?: continue
+                pixels.add(pixel)
             }
         }
 
@@ -71,10 +80,13 @@ class MultiMap {
     }
 
     fun number(): MultiMap {
-        for ((index, map) in matrix.flatten().withIndex()) {
+        val maps = maps() ?: return this
+
+        for ((index, map) in maps.flatten().withIndex()) {
             map.renderer.border(MapUtils.randomColor())
             map.renderer.centerText("$index")
         }
+
         return this
     }
 
