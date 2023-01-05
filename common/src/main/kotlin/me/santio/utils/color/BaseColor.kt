09cf07ff3d5d5971bb78ee908a8ed243ea97ea1c
Santio <admin@santio.me>
2023-01-04 21:26:20 -0500

    Add base colors

---
+++ common/src/main/kotlin/me/santio/utils/color/BaseColor.kt
@@ -0,0 +1,54 @@
+package me.santio.utils.color
+
+@Suppress("unused")
+object BaseColor {
+
+    @JvmStatic
+    val BLACK = Color.fromRGB(0, 0, 0)
+
+    @JvmStatic
+    val DARK_BLUE = Color.fromRGB(0, 0, 170)
+
+    @JvmStatic
+    val DARK_GREEN = Color.fromRGB(0, 170, 0)
+
+    @JvmStatic
+    val DARK_AQUA = Color.fromRGB(0, 170, 170)
+
+    @JvmStatic
+    val DARK_RED = Color.fromRGB(170, 0, 0)
+
+    @JvmStatic
+    val DARK_PURPLE = Color.fromRGB(170, 0, 170)
+
+    @JvmStatic
+    val GOLD = Color.fromRGB(255, 170, 0)
+
+    @JvmStatic
+    val GRAY = Color.fromRGB(170, 170, 170)
+
+    @JvmStatic
+    val DARK_GRAY = Color.fromRGB(85, 85, 85)
+
+    @JvmStatic
+    val BLUE = Color.fromRGB(85, 85, 255)
+
+    @JvmStatic
+    val GREEN = Color.fromRGB(85, 255, 85)
+
+    @JvmStatic
+    val AQUA = Color.fromRGB(85, 255, 255)
+
+    @JvmStatic
+    val RED = Color.fromRGB(255, 85, 85)
+
+    @JvmStatic
+    val LIGHT_PURPLE = Color.fromRGB(255, 85, 255)
+
+    @JvmStatic
+    val YELLOW = Color.fromRGB(255, 255, 85)
+
+    @JvmStatic
+    val WHITE = Color.fromRGB(255, 255, 255)
+
+}
