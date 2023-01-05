055f032f3b6ce35c1e297b14597d50313a9b81b4
Santio <admin@santio.me>
2023-01-04 21:27:47 -0500

    More color mappings

--- bukkit/src/main/kotlin/me/santio/utils/bukkit/impl/ColorImpl.kt
+++ bukkit/src/main/kotlin/me/santio/utils/bukkit/impl/ColorImpl.kt
@@ -1,7 +1,14 @@
+@file:JvmName("ColorUtils")
+@file:Suppress("unused")
+
 package me.santio.utils.bukkit.impl
 
 import me.santio.utils.color.Color
+import org.bukkit.map.MapPalette
 
 fun Color.toBukkitColor(): org.bukkit.Color = org.bukkit.Color.fromRGB(red, green, blue)
 fun Color.toSpigotColor(): net.md_5.bungee.api.ChatColor = net.md_5.bungee.api.ChatColor.of(java.awt.Color(red, green, blue))
 fun Color.toSpigotText() = "&${this.toHex()}"
+
+@Suppress("DEPRECATION")
+fun Color.toMapPalette() = MapPalette.matchColor(red, green, blue)
